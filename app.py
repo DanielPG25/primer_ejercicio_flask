7821e01e62af6ef87886ed28ae75a09af9bc02d8
Daniel Parrales García <dannyparrales16@gmail.com>
2021-04-08 12:49:01 +0200

    Creada la función para contar letras

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ def inicio():
 	return render_template("index.html")
 
 @app.route('/potencia/',methods=["GET","POST"])
-@app.route("/potencia/<int:base>/<int:exponente>",methods=["GET","POST"])
+@app.route("/potencia/<int:base>/<exponente>",methods=["GET","POST"])
 def potencia(base=1,exponente=2):
-	resultado = base**exponente
+	resultado = base**int(exponente)
 	return render_template("potencia.html",base=base,exponente=exponente,resultado=resultado)
 
+@app.route('/cuentaletras/',methods=["GET","POST"])
+@app.route("/cuenta/palabra/letra",methods=["GET","POST"])
+def cuentaletras(palabra="hola",letra="a"):
+	if len(letra) > 1:
+		abort(404)
+	veces=palabra.count(letra)
+	return render_template("cuentaletras.html",palabra=palabra,letra=letra,veces=veces)
 
 app.run(debug=True)
